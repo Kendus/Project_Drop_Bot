17987285e3cffc3b47c801dc7c721d1e8e49acd5
Kendus <kendustisdalejeffries@gmail.com>
2018-07-16 21:35:50 -0500

    Initial commit

---
+++ app/src/main/java/com/example/kendus/project_drop_bot/business2.java
@@ -0,0 +1,14 @@
+package com.example.kendus.project_drop_bot;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class business2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_business2);
+    }
+
+}
